c004ce49bdd18e5ef006160cb93309dbc7f41930
KrisdaKKU <66762533+KrisdaKKU@users.noreply.github.com>
2021-03-23 17:01:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/token")
+def my_function(text:str):
+  import nltk
+  nltk.download('punkt')
+  sent = text.lower()
+  list_word = nltk.word_tokenize(sent)
+  list_word = str(list_word)
+  return { "text":list_word }
