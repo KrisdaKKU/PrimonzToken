2eebf9642262a4c4172a90a1382c89bcecb7661e
KrisdaKKU <66762533+KrisdaKKU@users.noreply.github.com>
2021-04-21 02:31:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,3 @@ def my_function(text:str):
   list_word = str(list_word)
   return { "text":list_word }
 
-from fastapi import FastAPI
-from fastapi import UploadFile, File
-import uvicorn
-import predict
-
-app = FastAPI()
-
-@app.post("/api/predict")
-def predict_image(file:UploadFile = File(...)):
-  #print('aaa')
-  name = file.filename #.split('.')[-1] in ("jpg", "jpeg", "png","jfif")
-  char = predict.answer(name)
-  return str(char)
-
-if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run(app, host="localhost", port=2020, debug=True) 
